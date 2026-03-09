143e2fbbbc06daa13fefab590620e7ff83db6fb1
DriviHugo <hprado@drivimobility.com>
2026-03-09 13:01:38 +0100

    chore: add tst@aurea.com test user to seeds (Test123!)

--- prisma/seeds/dev/02_roles.js
+++ prisma/seeds/dev/02_roles.js
@@ -7,6 +7,10 @@ const data = [
     userId: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
     role: "processor",
   },
+  {
+    userId: "c83d1f2a-7b4e-4a91-b6c8-2e9f0d5a3b71",
+    role: "admin",
+  },
 ];
 
 export default {
